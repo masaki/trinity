9197fd184ed665694dff85f625c87afb9cf3b7bc
NAKAGAWA Masaki <masaki.nakagawa@gmail.com>
2009-03-23 20:54:49 +0900

    added display(), partial()

--- lib/Trinity/Controller/Role/Render.pm
+++ lib/Trinity/Controller/Role/Render.pm
@@ -3,6 +3,20 @@ package Trinity::Controller::Role::Render;
 use Mouse::Role;
 
 sub display {
+    my ($self, $object, @args) = @_;
+
+    my $body = $self->render(@args);
+    return $bodt if $body;
+
+    my %vars = @args;
+    if (@args % 2) {
+        shift @args;
+        %vars = @args;
+    }
+
+    my $format = $vars{format} || $self->req->format || 'html';
+    my $method = "to_${format}";
+    return $object->can($method) ? $object->$method : undef;
 }
 
 sub render {
@@ -13,7 +27,8 @@ sub render {
         my $thing = shift @args;
         %vars = @args;
 
-        if (blessed $thing) { # TODO: does('Trinity::View')
+        # FIXME: is "Trinity::Component::View" good name ?
+        if (blessed $thing and $thing->meta->does_role('Trinity::Component::View')) {
             # forward View
             return $thing->render(%vars);
         }
@@ -52,6 +67,33 @@ sub render {
 }
 
 sub partial {
+    my ($self, $template, %vars) = @_;
+
+    $template = do {
+        my @path = split m!/! => $template;
+        $path[-1] = "_$path[-1]";
+        join '/' => @path;
+    };
+    my $format = $vars{format} || $self->req->format || 'html';
+    my $file = do {
+        my @files;
+        my $base = "${template}\.${format}\.";
+        $self->path_to('templates')->recurse(
+            callback => sub {
+                my $file = shift;
+                push @files, $file if -f $file and $file =~ /$base/;
+            },
+        );
+        # TODO: templates selection
+        $files[0];
+    };
+    # TODO: exceptionize
+    unless ($file) {
+        die "Not found template $template.$format.*";
+    }
+
+    # TODO: render_with_template() is not implemented
+    $self->render_with_template($file, %vars);
 }
 
 no Mouse::Role;
