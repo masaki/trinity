b0a3f40eefb513c6b82f52c069a552c748a92552
NAKAGAWA Masaki <masaki.nakagawa@gmail.com>
2009-03-23 21:10:39 +0900

    refactored

--- lib/Trinity/Controller/Role/Render.pm
+++ lib/Trinity/Controller/Role/Render.pm
@@ -5,16 +5,13 @@ use Mouse::Role;
 sub display {
     my ($self, $object, @args) = @_;
 
-    my $body = $self->render(@args);
-    return $body if $body;
-
-    my %vars = @args;
-    if (@args % 2) {
-        shift @args;
-        %vars = @args;
+    if (my $body = $self->render(@args)) {
+        return $body;
     }
 
-    my $format = $vars{format} || $self->req->format || 'html';
+    my (undef, %vars) = $self->_populate_args(@args);
+
+    my $format = $self->_detect_format(%vars);
     my $method = "to_${format}";
     return $object->can($method) ? $object->$method : undef;
 }
@@ -22,22 +19,12 @@ sub display {
 sub render {
     my ($self, @args) = @_;
 
-    my %vars = @args;
-    if (@args % 2) {
-        my $thing = shift @args;
-        %vars = @args;
-
-        # FIXME: is "Trinity::Component::View" good name ?
-        if (blessed $thing and $thing->meta->does_role('Trinity::Component::View')) {
-            # forward View
-            return $thing->render(%vars);
-        }
-        else {
-            $vars{template} = $thing;
-        }
+    my ($view, %vars) = $self->_populate_args(@args);
+    if ($view) {
+        return $view->render(%vars);
     }
 
-    my $format = $vars{format} || $self->req->format || 'html';
+    my $format = $self->_detect_format(%vars);
     # forward View with provided
     # TODO: provided_view_for() is not implemented
     if (my $view = $self->provided_view_for($format)) {
@@ -94,6 +81,31 @@ sub partial {
 
     # TODO: render_with_template() is not implemented
     $self->render_with_template($file, %vars);
+}
+
+sub _populate_args {
+    my ($self, @args) = @_;
+    my ($view, %vars) = (undef, @args);
+
+    if (@args % 2) {
+        my $thing = shift @args;
+        %vars = @args;
+
+        # FIXME: is "Trinity::Component::View" good name ?
+        if (blessed $thing and $thing->meta->does_role('Trinity::Component::View')) {
+            $view = $thing;
+        }
+        else {
+            $vars{template} = $thing;
+        }
+    }
+
+    return ($view, %vars);
+}
+
+sub _detect_format {
+    my ($self, %vars) = @_;
+    return $vars{format} || $self->req->format || 'html';
 }
 
 no Mouse::Role;
