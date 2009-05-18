faeb24c6fd85220e2b4d87d465b614f3c5ae36ea
NAKAGAWA Masaki <masaki.nakagawa@gmail.com>
2009-05-18 20:50:56 +0900

    suppress export keywords

--- lib/Trinity/Application/Core/Path.pm
+++ lib/Trinity/Application/Core/Path.pm
@@ -2,8 +2,9 @@ package Trinity::Application::Core::Path;
 
 use Any::Moose '::Role';
 use Any::Moose 'X::Types::Path::Class';
-use Cwd qw(getcwd);
-use Path::Class qw(file dir);
+use Cwd ();
+use Path::Class::File ();
+use Path::Class::Dir ();
 use Trinity::Utils;
 
 has 'home' => (
@@ -12,16 +13,12 @@ has 'home' => (
     lazy_build => 1,
 );
 
-sub setup_layouts { shift->home }
-
 sub path_to {
     my ($self, @path) = @_;
 
-    my $path = dir($self->home, @path);
-    return -d $path ? $path : file($path);
+    my $path = Path::Class::Dir->new($self->home, @path);
+    return -d $path ? $path : Path::Class::File->new($path);
 }
-
-sub root { shift->path_to('root') }
 
 sub _build_home {
     my $self = shift;
@@ -29,7 +26,7 @@ sub _build_home {
     my $home;
     $home ||= $self->_setup_home_from_env;
     $home ||= $self->_setup_home_from_path;
-    $home ||= getcwd;
+    $home ||= Cwd::getcwd();
 
     return $home;
 }
@@ -39,7 +36,7 @@ sub _setup_home_from_env {
 
     return unless my $env = Trinity::Utils::env_value($self->meta->name, 'HOME');
 
-    my $home = dir($env)->absolute->cleanup;
+    my $home = Path::Class::Dir->new($env)->absolute->cleanup;
     return -d $home ? $home : undef;
 }
 
@@ -52,18 +49,11 @@ sub _setup_home_from_path {
     return unless my $path = $INC{$file};
     $path =~ s/$file$//;
 
-    my $home = dir($path)->absolute->cleanup;
+    my $home = Path::Class::Dir->new($path)->absolute->cleanup;
     $home = $home->parent while $home->dir_list(-1) =~ /^b?lib$/;
     $home = $home->parent->parent if ($home->dir_list(-1, 1))[0] eq '..';
 
     return -d $home ? $home : undef;
-}
-
-{
-    no strict 'refs';
-    for my $keyword (qw(file dir getcwd)) {
-        delete ${ __PACKAGE__ . '::' }{$keyword};
-    }
 }
 
 no Any::Moose '::Role';
